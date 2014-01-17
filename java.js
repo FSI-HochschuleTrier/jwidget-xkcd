cc783ffc6609d6dc5505b0050dab858b0dba5c30
Christopher Kaster <ikasoki@gmail.com>
2014-01-17 12:07:58 +0100

    add better image display

--- java.js
+++ java.js
@@ -41,16 +41,15 @@ function setup() {
 	panel_width = $("#xkcd").width();
 	panel_height = $("#xkcd").height();
 
+	load_image();
+}
+
+function load_image() {
 	// get newest xkcd
 	$.getJSON("http://anyorigin.com/get?url=http://xkcd.com/info.0.json&callback=?", function(data) {
 		var current_xkcd_number = data.contents.num;
 		
-		var random_number = Math.round(Math.random()*current_xkcd_number);
-		
-		random_number = random_number > 0 ?
-			random_number : random_number + 1;
-		random_number = random_number < current_xkcd_number ?
-			random_number : random_number - 1;
+		var random_number = generate_random_number(current_xkcd_number);
 			
 		// only use the following line for testing purpose
 		//random_number = 1212;
@@ -61,91 +60,46 @@ function setup() {
 		$.getJSON(url, function(data) {
 			$("#xkcd-title").text(data.contents.title + " #" + data.contents.num);
 			$("#xkcd-footer").text(data.contents.alt);
-			on_image_url_found(data.contents.img);
+			
+			retrieve_image_size(data.contents.img, function(size) {
+				on_image_url_found(data.contents.img, size);
+			});
 		});
 	});
-
-	
 }
 
-function on_image_url_found(url) {
-	// set image
-	$("#xkcd-image").attr("src", url);
-	
-	setTimeout(start, 1000);
-}
-
-function start() {
-	interval = setInterval(scroll_comic, 100);
-}
-
-function wait_and_call(func) {
-	setTimeout(func, 1000);
-}
-
-function reset() {
-	margin_top = 0;
-	margin_left = 0;
-	
-	$("#xkcd-image").css({
-		"margin-top": margin_top,
-		"margin-left": margin_left
-	});
-	
-	setTimeout(start, 1000);
-}
-
-function scroll_comic() {
-	var image_width = $("#xkcd-image").width();
-	var image_height = $("#xkcd-image").height();
+function on_image_url_found(url, size) {
+	var image_width = size.width;
+	var image_height = size.height;
 	var footer_height = $("#xkcd-footer").height();
 	
-	var usable_panel_height = panel_height - footer_height;
-	
-	if(image_width > panel_width) {
-		if(image_width + margin_left <= panel_width) {
-			if(!aligned_to_bottom) {
-				margin_left = 0;
-				
-				if(image_height > usable_panel_height) {
-					margin_top -= panel_height;
-				}
-			}
-			
-			if(!aligned_to_bottom && Math.abs(margin_top) > Math.abs(image_height - usable_panel_height)) { 
-				margin_top = Math.abs(image_height - usable_panel_height) * -1;
-				console.log(margin_top);
-				aligned_to_bottom = true;
-			} else {
-				aligned_to_bottom = false;
-			}
-			
-			if(!aligned_to_bottom && image_height + margin_top <= usable_panel_height) {
-				clearInterval(interval);
-			
-				wait_and_call(function() {
-					reset();
-				});
-			}
-		} else {
-			margin_left -= 2;
-		}
-	} else if(image_height > panel_height){
-		console.log(footer_height);
-	
-		if(image_height + margin_top <= usable_panel_height) {
-			clearInterval(interval);
-			
-			wait_and_call(function() {
-				reset();
-			});
-		} else {
-			margin_top -= 2;
-		}
+	// check if image is smaller than the panel
+	if(image_width <= panel_width && image_height <= (panel_height - footer_height)) {	
+		console.log(image_width + " <= " + panel_width);
+		
+		// set image
+		$("#xkcd-image").attr("src", url);
+	} else {
+		// TODO: mark image as too big
+		load_image();
 	}
+}
 
-	$("#xkcd-image").css({
-		"margin-top": margin_top,
-		"margin-left": margin_left
-	});
+function retrieve_image_size(url, callback) {
+	$("<img/>").attr("src", url).load(function() {
+		var size = {width:this.width, height:this.height};
+		
+		callback(size);
+	}); 
+}
+
+function generate_random_number(max) {
+	var random_number = Math.round(Math.random() * max);
+		
+	random_number = random_number > 0 ?
+		random_number : random_number + 1;
+	random_number = random_number < max ?
+		random_number : random_number - 1;
+		
+	return random_number;
 }
